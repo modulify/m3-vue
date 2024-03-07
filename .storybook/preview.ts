91939a79545247d4bce83482c82c5c64a3658698
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2024-03-07 21:44:54 +0400

    docs: Disabled backgrounds control, stories sorting

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -7,11 +7,15 @@ import { withThemeByClassName } from '@storybook/addon-themes'
 
 export default {
   parameters: {
+    backgrounds: { disable: true },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    options: {
+      storySort: (a, b) => a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
     },
   },
   decorators: [
